08e19b1ca82c9b443e862ffdbad8593f6b57c765
Suvitha <suviiiramesh@gmail.com>
2025-08-08 15:49:03 +0530

    added

---
+++ threestringfirstletter.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class threestringfirstletter {
+    public static void main(String[] args) {
+        System.out.println("Enter three strings:");
+        Scanner string = new Scanner(System.in);
+        String str1 = string.next();
+        String str2 = string.next();
+        String str3 = string.next();
+        System.out.print("First String"+" " + str1);
+        char ch = string.next().charAt(0);
+        if (str1.charAt(0) == ch || str2.charAt(0) == ch || str3.charAt(0) == ch) {
+            System.out.print("matched");
+
+        }
+        else{
+            System.out.print("not matched");
+        }
+            
+    }
+}
+
+        
+
